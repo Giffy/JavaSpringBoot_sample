921f04e9fc9e2d72f0b03438fcba775ec1e7a5fc
Giffy <1595343+Giffy@users.noreply.github.com>
2020-09-30 16:04:21 +0200

    Update InvalidParamException.java

--- src/main/java/com/whitecollar/project/utilities/InvalidParamException.java
+++ src/main/java/com/whitecollar/project/utilities/InvalidParamException.java
@@ -3,7 +3,7 @@ package com.whitecollar.project.utilities;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason = "Información inválida")
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason = "Informacion invalida")
 public class InvalidParamException extends Exception {
 
 	public InvalidParamException() {
